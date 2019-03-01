c79d2f0598335d23b06d84b81c21ed01f0fc49ea
Your Name <you@example.com>
2019-03-01 10:40:09 +0530

    Scrapping titles and svgs

--- scraper.js
+++ scraper.js
@@ -5,7 +5,15 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   const url = 'https://marketingplatform.google.com/about/';
   await page.goto(url);
-  await page.screenshot({path: 'example.png'});
+  // await page.screenshot({path: 'example.png'});
+  const titles = await page.evaluate(
+    () => Array.from(document.querySelectorAll('h4.h-c-tile__headline, h4.product-card__headline')).map((heading) => heading.innerText)
+  );
 
+  const svgs = await page.evaluate(
+    () => Array.from(document.querySelectorAll('div.h-c-tile__header picture svg')).map((svg) => svg.id)
+  );
+  console.log(titles);
+  console.log(svgs);
   await browser.close();
 })();
